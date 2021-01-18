9c7c1b53ca0553eb9f8d25ba2e5cf4c07ef2d10d
VitalieVSV <dut66AHJCT>
2021-01-18 10:37:19 +0200

    Button added

--- src/GUI/MainLayout.java
+++ src/GUI/MainLayout.java
@@ -1,10 +1,24 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainLayout extends JFrame {
     public MainLayout() {
         this.setTitle("TextApplication");
-        
+        this.setAlwaysOnTop(true);
+        this.setPreferredSize(new Dimension(100, 200));
+        this.setVisible(true);
+        initializeComponents();
+    }
+
+    public void initializeComponents() {
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 0, 100, 200);
+        panel.setBackground(Color.gray);
+        JButton readFileButton = new JButton("Read From File");
+        readFileButton.setBounds(0,0,80,30);
+        panel.add(readFileButton);
+        this.add(panel);
     }
 }
---
+++ src/MainApplication/MainInitializer.java
@@ -0,0 +1,9 @@
+package MainApplication;
+
+import GUI.MainLayout;
+
+public class MainInitializer {
+    public static void main(String[] args) {
+        new MainLayout();
+    }
+}
